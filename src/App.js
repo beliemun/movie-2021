22fe1e2f903329fa820546915a0b64b8b012df3a
beliemun <beliemun@paran.com>
2021-01-15 18:58:02 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: [],
+  };
+
+  getMovie = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json");
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovie();
+  }
+
+  renderMovies = (movies) => {
+    return movies.map((movie) => {
+      return (
+        <Movie
+          key={movie.id}
+          id={movie.id}
+          year={movie.year}
+          title={movie.title}
+          summary={movie.summary}
+          poster={movie.medium_cover_image}
+          genres={movie.genres}
+        />
+      );
+    });
+  };
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading.. </span>
+          </div>
+        ) : (
+          <div className="movies">{this.renderMovies(movies)}</div>
+        )}
+      </section>
+    );
+  }
+}
+export default App;
